da243f6f59e07c42a972fcaa0cd1df2b2a22a4e2
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-08-10 01:32:34 -0500

    Added #create tests to pokemons controller spec

--- spec/controllers/pokemons_controller_spec.rb
+++ spec/controllers/pokemons_controller_spec.rb
@@ -18,4 +18,45 @@ RSpec.describe PokemonsController, type: :controller do
       expect(response.json).to eq json
     end
   end
+
+  describe '#create' do
+
+    context 'valid params' do
+
+      let(:params) do
+        attributes_for(:pokemon, image: test_image_base64_string)
+      end
+
+      let(:json) do
+        Pokemon.last.as_json(only: %i(id name), methods: %i(image_url))
+      end
+
+      before { post :create, params: { pokemon: params } }
+
+      it { should respond_with :created }
+
+      it 'should render expected JSON' do
+        expect(response.json).to eq json
+      end
+    end
+
+    context 'invalid params' do
+
+      let(:params) do
+        attributes_for(:pokemon, name: '')
+      end
+
+      let(:json) do
+        { name: ["can't be blank"] }.with_indifferent_access
+      end
+
+      before { post :create, params: { pokemon: params } }
+
+      it { should respond_with :unprocessable_entity }
+
+      it 'should render expected JSON' do
+        expect(response.json).to eq json
+      end
+    end
+  end
 end
