692698caace1f84b78ce7006d9c829d9fb89a395
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-08-10 01:33:17 -0500

    Added #create to pokemons controller

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,12 +1,29 @@
 class PokemonsController < ApplicationController
 
-  before_action :load_pokemon, only: %i(show)
+  before_action :build_pokemon, only: %i(create)
+  before_action :load_pokemon,  only: %i(show)
+
+  def create
+    if @pokemon.save
+      render json: @pokemon, status: :created
+    else
+      render json: @pokemon.errors, status: :unprocessable_entity
+    end
+  end
 
   def show
     render json: @pokemon
   end
 
   private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :image)
+  end
+
+  def build_pokemon
+    @pokemon = Pokemon.new pokemon_params
+  end
 
   def load_pokemon
     @pokemon = Pokemon.find params[:id]
